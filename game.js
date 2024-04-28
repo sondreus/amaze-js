41d5686ffb3dff576de1266a87411c1324f77a68
Sondre Ulvund Solstad <sondresolstad@economist.com>
2024-04-28 17:57:11 +0100

    sharing notification removed

--- game.js
+++ game.js
@@ -303,6 +303,32 @@ function shareMap() {
   } else {
     // Fallback to copying the link to the clipboard
     navigator.clipboard.writeText(shareLink);
-    alert('Copied to clipboard!');
+    // Change the button icon or color
+    changeShareButtonState();
   }
+}
+
+function changeShareButtonState() {
+  const shareButton = document.querySelector('.share-button');
+
+  // Change the button icon
+  shareButton.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+      <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+    </svg>
+  `;
+
+  // Change the button color
+  shareButton.classList.add('text-green-500');
+
+  // Reset the button state after a certain duration
+  setTimeout(() => {
+    shareButton.innerHTML = `
+      <svg id="Footer-module_shareIcon__wOwOt" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 24 24" width="20" class="game-icon" data-testid="icon-share">
+        <path fill="var(--white)" d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92s2.92-1.31 2.92-2.92c0-1.61-1.31-2.92-2.92-2.92zM18 4c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zM6 13c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm12 7.02c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z">
+        </path>
+      </svg>
+    `;
+    shareButton.classList.remove('text-green-500');
+  }, 2000);
 }
